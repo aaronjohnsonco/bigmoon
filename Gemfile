d751a9a2935b7b4b7096ab59fb7f303abdc01235
Aaron Johnson <aaronjohnsonis@gmail.com>
2017-06-16 07:14:46 -0500

    add recaptcha

--- Gemfile
+++ Gemfile
@@ -52,6 +52,9 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+gem 'dotenv-rails', :require => 'dotenv/rails-now'
+
+gem 'recaptcha', require: 'recaptcha/rails'
 
 gem 'rails_12factor', group: :production
 gem 'pg', group: :production
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,11 +2,21 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.create(contact_params)
-    if @contact.save
-      ContactMailer.new_submission_email(@contact).deliver
-      ContactMailer.thank_you_email(@contact).deliver
-      redirect_to root_path
+    respond_to do |format|
+      if verify_recaptcha(model: @contact) && @contact.save
+          ContactMailer.new_submission_email(@contact).deliver
+          ContactMailer.thank_you_email(@contact).deliver
+        format.html { redirect_to root_path }
+        format.json
+      else
+        format.html { redirect_to root_path }
+      end
     end
+    # if @contact.save
+    #   ContactMailer.new_submission_email(@contact).deliver
+    #   ContactMailer.thank_you_email(@contact).deliver
+    #   redirect_to root_path
+    # end
   end
 
   private
