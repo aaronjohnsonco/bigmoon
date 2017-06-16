c28ea15c3bab547b5f2c9f1085bd06745b91222e
Aaron Johnson <aaronjohnsonis@gmail.com>
2017-06-16 13:17:29 -0500

    add flash message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,16 +2,16 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.create(contact_params)
-    respond_to do |format|
-      if verify_recaptcha(@contact) && @contact.save
-          ContactMailer.new_submission_email(@contact).deliver
-          ContactMailer.thank_you_email(@contact).deliver
-        format.html { redirect_to root_path }
-        format.json
-      else
-        format.html { redirect_to root_path }
-      end
+    if verify_recaptcha(@contact) && @contact.save
+      ContactMailer.new_submission_email(@contact).deliver
+      ContactMailer.thank_you_email(@contact).deliver
+      redirect_to root_path
+      flash[:notice] = "Your message has been sent."
+    else
+      redirect_to root_path
+      flash[:notice] = "Your email did not go through, please try again."
     end
+    
     # if @contact.save
     #   ContactMailer.new_submission_email(@contact).deliver
     #   ContactMailer.thank_you_email(@contact).deliver
